ea652619293b7a7ff78a9b792264746ead61c84c
Your Name <liangj@rose-hulman.edu>
2026-04-02 15:36:30 -0400

    debug

--- Software2/src/normal_mode.c
+++ Software2/src/normal_mode.c
@@ -112,7 +112,7 @@ bool normal_mode_handle_key(char key, bool is_hold, bool is_shifted) {
         int vox = radio_get_vox_status();
 
         if (vox < 0) {
-            speech_say_text("VOX status unavailable");
+            speech_say_text("VOX status unavailable, shift one pressed");
             } else if (vox == 1) {
             speech_say_text("VOX is now on");
             } else {
