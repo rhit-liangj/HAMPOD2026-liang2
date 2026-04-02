a54013a73a40ef795463d5b550f05aa59aee6719
Your Name <liangj@rose-hulman.edu>
2026-04-02 16:02:00 -0400

    check

--- Software2/src/set_mode.c
+++ Software2/src/set_mode.c
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 // ============================================================================
 // Module State
