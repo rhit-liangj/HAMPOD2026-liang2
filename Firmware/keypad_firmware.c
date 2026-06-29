4028856f9d1c60d3a973128128e2b10e4f94bc89
Your Name <liangj@rose-hulman.edu>
2026-06-29 10:42:47 -0400

    testing

--- Firmware/keypad_firmware.c
+++ Firmware/keypad_firmware.c
@@ -165,7 +165,8 @@ while (keypad_running) {
     if (key_pressed) {
 
         clock_gettime(CLOCK_MONOTONIC, &t_keypad_after_write);
-
+        KEYPAD_PRINTF("[LATENCY][KEYPAD] intializationtime: %.3f ms\n",
+              elapsed_ms(start, end));
         KEYPAD_PRINTF(
             "[LATENCY][KEYPAD] Key '%c' Total firmware latency: %.3f ms\n",
             read_value,
